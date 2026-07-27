eca959d235620715ec70ff95389bb00e2065228f
DianaMoschetti <dianamoschetti@gmail.com>
2026-07-27 07:44:53 -0300

    Refactor 2.1 - Data: Agregar config de la nueva entidad puntos por cargo

---
+++ DocentesApp.Data/Configurations/PuntosPorCargoConfig.cs
@@ -0,0 +1,23 @@
+using DocentesApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DocentesApp.Data.Configurations
+{
+    public class PuntosPorCargoConfig : IEntityTypeConfiguration<PuntosPorCargo>
+    {
+        public void Configure(EntityTypeBuilder<PuntosPorCargo> builder)
+        {
+            builder.ToTable("PuntosPorCargo");
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Denominacion).IsRequired();
+            builder.Property(p => p.TipoCargo).IsRequired();
+            builder.Property(p => p.Condicion).IsRequired();
+            builder.Property(p => p.PuntosBase).IsRequired().HasPrecision(10, 2);
+
+            builder.HasIndex(p => new { p.Denominacion, p.TipoCargo, p.Condicion })
+                .IsUnique();
+        }
+    }
+}
